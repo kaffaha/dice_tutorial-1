bbc3e6862d214716452a688568205f6dd363f77c
kaffaha <rifqy.kaffaha@gmail.com>
2024-02-19 10:03:07 +0700

    first commit

---
+++ first_project/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:first_project/gradient_container.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          Color.fromARGB(255, 32, 0, 86),
+          Color.fromARGB(255, 18, 1, 30),
+        ),
+      ),
+    ),
+  );
+}
